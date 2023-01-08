bb7b5ee1019e3d0e2a5c91bdbdd3936d4773e74a
redcherrytree <38h4cero@gmail.com>
2023-01-08 19:18:31 +0100

    Innit Commit

---
+++ src/main/java/net/redcherrytree/testmod/TestModClient.java
@@ -0,0 +1,11 @@
+package net.redcherrytree.testmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
